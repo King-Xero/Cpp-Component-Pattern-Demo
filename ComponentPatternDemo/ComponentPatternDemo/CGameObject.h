39eb481db05da1bd05be6531b0e0c772cbe28eba
Toyan Green <toyangreen@gmail.com>
2019-09-28 11:44:02 +0100

    Added GameObject class that mirrors the Character class

---
+++ ComponentPatternDemo/ComponentPatternDemo/CGameObject.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "SCollisionBounds.h"
+#include "CSprite.h"
+
+class CGraphics;
+class CCollisionSystem;
+
+class CGameObject
+{
+public:
+	CGameObject() : m_iVelocity(0), m_iX(0), m_iY(0) {  }
+
+	void Update(CCollisionSystem& collisionSystem, CGraphics& graphics);
+
+private:
+	static const int sk_iMOVEMENT_ACCELERATION = 3;
+	int m_iVelocity;
+	int m_iX;
+	int m_iY;
+
+	SCollisionBounds m_CollisionBounds;
+
+	CSprite m_SpriteIdle;
+	CSprite m_SpriteWalkRight;
+	CSprite m_SpriteWalkLeft;
+};
